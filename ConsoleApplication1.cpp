28ea62047a7f5bf6fd906b6d325b2c10e10f05fe
José Santizo Galeano <ricardosantizo@meso.edu>
2023-02-14 07:59:15 -0600

    subi mi ejercicio

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+// Primerproyecto Richard.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+
+using namespace std;
+
+
+int main() //estoes un comentario 
+{
+    std::cout << "Jose Ricardo Santizo Galeano\n";
+    std::cout << "202307030\n";
+    std::cout << "Porque me gusta y quiero crear muchas cosas para mi o el mundo\n";
+    system("pause");
+    return 0;
+}
